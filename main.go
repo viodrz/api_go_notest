a33d157e26977357643bc5f7ff26f87899aeafe0
Nova Novriansyah <nov_rian@yahoo.com>
2024-11-14 14:27:07 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,9 +8,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World v2.1")
 
-	fmt.Fprintf(w, "modif by deva")
-
-	fmt.Fprintf(w, "modif by dev 2")
+	 
 
 }
 func main() {
